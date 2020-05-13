ee1607048a07756dd5af4c03b1828002a22ae511
Aleksandr <aleksandr@mistchenko.ru>
2020-05-13 14:11:15 +0400

    chown nobody nogroup

--- app/server.py
+++ app/server.py
@@ -1,7 +1,12 @@
 import os
+import pwd
+import grp
 from flask import Flask, request, render_template
 from werkzeug.utils import secure_filename
 from datetime import date
+
+uid = pwd.getpwnam("nobody").pw_uid
+gid = grp.getgrnam("nogroup").gr_gid
 
 app = Flask(__name__,
             static_url_path='',
@@ -24,9 +29,12 @@ def index():
             filename = secure_filename(f.filename)
             dir = os.path.join(app.config['UPLOAD_FOLDER'], subdir)
             os.makedirs(dir, exist_ok=True)
+            os.chmod(dir, 0o777)
+            os.chown(dir, uid, gid)
             full_path = os.path.join(dir, filename)
             f.save(full_path)
             os.chmod(full_path, 0o666)
+            os.chown(full_path, uid, gid)
             up_files.append(filename)
             print('save:', filename)
 
